9117c2c3e8e152e617ee5de31ee416e016aaa6da
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-09-23 12:16:42 +0530

    added align to chats

--- components/chat.tsx
+++ components/chat.tsx
@@ -55,7 +55,7 @@ export default function Chat({
         >
           <div className={`w-5/6 flex flex-col gap-2 border p-4 rounded-lg`}>
             <div
-              className={`flex flex-col ${user?.full_name === query?.user?.full_name ? "md:flex-row-reverse" : "md:flex-row"}  items-center justify-between`}
+              className={`flex flex-col ${user?.full_name === query?.user?.full_name ? "items-end md:flex-row-reverse" : "items-start md:flex-row"}  md:items-center justify-between`}
             >
               <span className="font-semibold">{query?.user?.full_name}</span>
               <span className="text-muted-foreground text-sm">
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-8 items-center">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 pt-8 md:pt-0 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Query
       </h1>
       <h2 className="scroll-m-20 pb-2 md:text-3xl font-semibold text-center tracking-tight first:mt-0">
@@ -9,7 +9,7 @@ export default function Header() {
       </h2>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent" />
       <p className="leading-7 md:w-3/6 text-center [&:not(:first-child)]:mt-6">
-        sleek app integrated with OpenAI that delivers precise, concise answers
+        sleek app integrated with GROQ AI that delivers precise, concise answers
         to your questions. Forget lengthy explanations and get straight to the
         point with instant, short responses tailored to your needs.
       </p>
